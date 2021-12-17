b235f1413e8121481935b823568f4f53f48bc0d3
GiantTree <GiantTreeLP@users.noreply.github.com>
2021-12-17 21:43:16 +0100

    Initial commit

---
+++ src/main/java/de/gianttree/mc/chestsorter/ChestSorter.kt
@@ -0,0 +1,14 @@
+package de.gianttree.mc.chestsorter
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ChestSorter : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
