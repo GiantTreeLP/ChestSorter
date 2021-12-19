a28613edb8d0e69c81992da046562d8cb83bf3e7
GiantTree <GiantTreeLP@users.noreply.github.com>
2021-12-19 22:25:46 +0100

    Sort items with custom names in lexicographically in front of the ones without

--- src/main/java/de/gianttree/mc/chestsorter/commands/internal/ItemStackComparator.kt
+++ src/main/java/de/gianttree/mc/chestsorter/commands/internal/ItemStackComparator.kt
@@ -1,14 +1,27 @@
 package de.gianttree.mc.chestsorter.commands.internal
 
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.Damageable
 import org.bukkit.inventory.meta.EnchantmentStorageMeta
+import java.util.function.Function
 
 object ItemStackComparator : Comparator<ItemStack> {
     private val comparator = Comparator.nullsLast(
         Comparator.comparingInt<ItemStack> {
             it.type.ordinal
-        }.thenComparingInt {
+        }.thenComparing(Function<ItemStack, String> {
+            return@Function if (it.hasItemMeta()) {
+                val meta = it.itemMeta
+                if (meta.hasDisplayName()) {
+                    meta.displayName()?.let(PlainTextComponentSerializer.plainText()::serialize)
+                } else {
+                    null
+                }
+            } else {
+                null
+            }
+        }, Comparator.nullsLast(Comparator.comparing(Function.identity()))).thenComparingInt {
             -it.amount
         }.thenComparingInt {
             if (it.hasItemMeta()) {
